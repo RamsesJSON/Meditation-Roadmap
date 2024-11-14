8d4cc90ab7c0eb4230836c0077f486bb3887aa11
RamsesJSON <113996692+RamsesJSON@users.noreply.github.com>
2024-11-14 11:29:46 +0000

    Deploying to gh-pages from @ RamsesJSON/Meditation-Roadmap@c0e3582226878184495f9967042c1f3e46f4e638 🚀

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC4YOGShr6yVYNSZZnqDgQzr04wrCAti5Y',
+  authDomain: 'meditatetogether-d4786.firebaseapp.com',
+  databaseURL: 'https://meditatetogether-d4786-default-rtdb.firebaseio.com',
+  projectId: 'meditatetogether-d4786',
+  storageBucket: 'meditatetogether-d4786.firebasestorage.app',
+  messagingSenderId: '631942692599',
+  appId: '1:631942692599:web:4f58f06b4d64d434691df2',
+  measurementId: ''
+};
